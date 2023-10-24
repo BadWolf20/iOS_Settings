5c037b39d3df4e0cf0f73e971df47b4fdae59f78
BadWolf20 <Roman.Pichugin20@gmail.com>
2023-10-24 13:20:17 +0700

    Initial Commit

---
+++ iOS_Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Settings
+//
+//  Created by Roman on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
